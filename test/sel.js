02b0cc559fd6d3e2c4b91d3fcd3eab3dcabac7e3
Princu7 <princu7@gmail.com>
2017-05-24 14:13:54 +0530

    Please God

--- test/sel.js
+++ test/sel.js
@@ -4,32 +4,31 @@ var assert = require("assert");
 var webdriver = require("selenium-webdriver");
 
 describe("testing javascript in the browser", function() {
-  this.timeout(60000);
-  if (process.env.SAUCE_USERNAME != undefined) {
-    this.browser = new webdriver.Builder()
-      .usingServer('http://'+ process.env.SAUCE_USERNAME+':'+process.env.SAUCE_ACCESS_KEY+'@ondemand.saucelabs.com:80/wd/hub')
-      .withCapabilities({
-        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-        build: process.env.TRAVIS_BUILD_NUMBER,
-        username: process.env.SAUCE_USERNAME,
-        accessKey: process.env.SAUCE_ACCESS_KEY,
-        browserName: "chrome"
-      }).build();
-  } else {
-    this.browser = new webdriver.Builder()
-      .withCapabilities({
-        browserName: "chrome"
-      }).build();
-  }
+  beforeEach(function() {
+    if (process.env.SAUCE_USERNAME != undefined) {
+      console.log("Inside if condition");
+      this.browser = new webdriver.Builder()
+        .usingServer('http://'+ process.env.SAUCE_USERNAME+':'+process.env.SAUCE_ACCESS_KEY+'@ondemand.saucelabs.com:80/wd/hub')
+        .withCapabilities({
+          'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+          build: process.env.TRAVIS_BUILD_NUMBER,
+          username: process.env.SAUCE_USERNAME,
+          accessKey: process.env.SAUCE_ACCESS_KEY,
+          browserName: "chrome"
+        }).build();
+    } else {
+      this.browser = new webdriver.Builder()
+        .withCapabilities({
+          browserName: "chrome"
+        }).build();
+    }
 
-
-  afterEach(function() {
-    return this.browser.quit();
+    return this.browser.get("http://google.com");
   });
 
   it("should handle clicking on a headline", function(done) {
-    driver.get('https://www.google.com');
-    driver.getTitle().then(function (title) {
+    console.log("Inside it condition");
+    browser.getTitle().then(function (title) {
       console.log("title is: " + title);
       done();
     });
