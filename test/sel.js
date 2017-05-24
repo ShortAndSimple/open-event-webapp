a40cd34303e8fc2cdba1f6b29d9e55132b361592
Princu7 <princu7@gmail.com>
2017-05-24 14:32:01 +0530

    I am tired

--- test/sel.js
+++ test/sel.js
@@ -1,35 +1,37 @@
-var app = require('../src/app');
-//console.log(app);
 var assert = require("assert");
 var webdriver = require("selenium-webdriver");
 
 describe("testing javascript in the browser", function() {
+  this.timeout(60000);
   beforeEach(function() {
     if (process.env.SAUCE_USERNAME != undefined) {
-      console.log("Inside if condition");
       this.browser = new webdriver.Builder()
-        .usingServer('http://'+ process.env.SAUCE_USERNAME+':'+process.env.SAUCE_ACCESS_KEY+'@ondemand.saucelabs.com:80/wd/hub')
-        .withCapabilities({
-          'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-          build: process.env.TRAVIS_BUILD_NUMBER,
-          username: process.env.SAUCE_USERNAME,
-          accessKey: process.env.SAUCE_ACCESS_KEY,
-          browserName: "chrome"
-        }).build();
+      .usingServer('http://'+ process.env.SAUCE_USERNAME+':'+process.env.SAUCE_ACCESS_KEY+'@ondemand.saucelabs.com:80/wd/hub')
+      .withCapabilities({
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+        build: process.env.TRAVIS_BUILD_NUMBER,
+        username: process.env.SAUCE_USERNAME,
+        accessKey: process.env.SAUCE_ACCESS_KEY,
+        browserName: "chrome"
+      }).build();
     } else {
       this.browser = new webdriver.Builder()
-        .withCapabilities({
-          browserName: "chrome"
-        }).build();
+      .withCapabilities({
+        browserName: "chrome"
+      }).build();
     }
 
     return this.browser.get("http://google.com");
   });
 
+  afterEach(function() {
+    return this.browser.quit();
+  });
+
   it("should handle clicking on a headline", function(done) {
-    console.log("Inside it condition");
-    browser.getTitle().then(function (title) {
-      console.log("title is: " + title);
+    var headline = this.browser.findElement(webdriver.By.css('h1'));
+    headline.getText().then(function(txt) {
+      console.log(txt);
       done();
     });
   });
