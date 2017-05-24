05fe3a85dd78583b3af8207e9ea37081c0d11122
Princu7 <princu7@gmail.com>
2017-05-24 14:05:04 +0530

    Please work

--- test/sel.js
+++ test/sel.js
@@ -4,6 +4,7 @@ var assert = require("assert");
 var webdriver = require("selenium-webdriver");
 
 describe("testing javascript in the browser", function() {
+  this.timeout(60000);
   if (process.env.SAUCE_USERNAME != undefined) {
     this.browser = new webdriver.Builder()
       .usingServer('http://'+ process.env.SAUCE_USERNAME+':'+process.env.SAUCE_ACCESS_KEY+'@ondemand.saucelabs.com:80/wd/hub')
